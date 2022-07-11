ce819a8c7660d6d395fa9df7f4a91cd2528bffce
David <david.programming@protonmail.com>
2022-07-11 19:53:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### Simple Pomodoro Timer
+- the demo can be used [here](https://github.com/davidk55/simple-pomodoro-timer).
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const mode =
+  process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+// const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: mode,
+  // entry: {
+  //   main: path.resolve(__dirname, 'src/main.js')
+  // },
+  // output: {
+  //   path: path.resolve(__dirname, 'dist'),
+  //   filename: '[name][contenthash].js',
+  //   clean: true,
+  //   assetModuleFilename: '[name][ext]'
+  // },
+
+  devtool: 'source-map',
+  devServer: {
+    static: 'dist',
+    // port: 3000,
+    open: true,
+    hot: true,
+    // compress: true,
+    // historyApiFallback: true,
+    watchFiles: ['src/**/*'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: "" }
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Simple Pomodoro Timer',
+      filename: './index.html',
+      template: './src/template.html',
+      favicon: './src/assets/favicon.svg'
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
